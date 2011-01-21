9fbd45d12265c0ca39cd2c8a03e62abaea82a095
Rafe Kettler <rafe.kettler@gmail.com>
2011-01-21 15:57:23 -0500

    Added callable class example

--- magicmethods.py
+++ magicmethods.py
@@ -105,3 +105,17 @@ class FunctionalList:
     def take(self, n):
         # get first n elements
         return self.values[:n]
+
+# Entity class demonstrating __call__
+class Entity:
+    '''Class to represent an entity. Callable to update the entity's position.'''
+
+    def __init__(self, size, x, y):
+        self.x, self.y = x, y
+        self.size = size
+
+    def __call__(self, x, y):
+        '''Change the position of the entity.'''
+        self.x, self.y = x, y
+    
+    # snip...
