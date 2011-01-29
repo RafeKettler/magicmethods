5045a00ebea6e37ff2ed85ff4f2eea034b2278fb
Rafe Kettler <rafe.kettler@gmail.com>
2011-01-29 14:10:39 -0500

    magicmarkdown.py script for conversions, fully markdownified magicmethods.mkd, now ready for forking and editing :)

---
+++ magicmarkdown.py
@@ -0,0 +1,10 @@
+# magicmarkdown.py
+# utility script for changing markdown from magic methods guide into HTML
+
+import markdown
+
+mkd = open('magicmethods.mkd', 'r')
+text = mkd.read()
+html = markdown.markdown(text, ['def_list'])
+out = open('markedup.html', 'w')
+out.write(html)
