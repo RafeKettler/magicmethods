3fec50508cf852b7516d689dda50a68cbdcd49e1
Rafe Kettler <rafe.kettler@gmail.com>
2011-01-30 23:52:41 -0500

    New appendix detailing syntax to invoke methods.
    This includes a good deal of associated changes -- magicmarkdown.py has changed to markup the appendix, new appendix.mkd file containing the appendix, and CSS rules for tables.

--- magicmarkdown.py
+++ magicmarkdown.py
@@ -5,7 +5,14 @@ import markdown
 
 mkd = open('magicmethods.mkd', 'r')
 text = mkd.read()
+mkd.close()
+mkd2 = open('appendix.mkd', 'r')
+text2 = mkd2.read()
+mkd2.close()
 html = markdown.markdown(text, 
                          ['def_list', 'codehilite'])
+appendix = markdown.markdown(text2, ['tables'])
 out = open('markedup.html', 'w')
 out.write(html)
+out.write(appendix)
+out.close()
